1a652f3e07679c87d786bfbbb7088fdb03e7a917
Kevin Adams <adams.kevin@student.greenrive.edu>
2023-06-06 14:36:57 -0700

    Adding inititial files just a hello world example

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,12 @@
+/**
+ *
+ * @author Kevin
+ * @version 1.0
+ */
 public class Main {
+    /**
+     * @param args argument
+     */
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
